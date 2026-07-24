728f6e515fb625045efe66f190374ca9e03ce944
carlosg <carlosg@users.noreply.github.com>
2026-07-24 14:04:58 +0200

    deploy v232 NVIDIA como IA de nube

--- assets/index-BJIo5D7y.js
+++ assets/index-BJIo5D7y.js
@@ -1,4 +1,4 @@
-import{a as is}from"./index-Bc543Nj2.js";/*! *****************************************************************************
+import{a as is}from"./index-DhtiOlwI.js";/*! *****************************************************************************
 Copyright (c) Microsoft Corporation.
 
 Permission to use, copy, modify, and/or distribute this software for any
