d9ab2acccfad841bcb8243eb3d39aee62f6e26d2
carlosg <carlosg@users.noreply.github.com>
2026-07-24 12:46:20 +0200

    deploy v231 cuaderno IA motores

--- assets/index-BC8hhLRj.js
+++ assets/index-BC8hhLRj.js
@@ -1,4 +1,4 @@
-import{a as is}from"./index-B9x8k7_R.js";/*! *****************************************************************************
+import{a as is}from"./index-Bc543Nj2.js";/*! *****************************************************************************
 Copyright (c) Microsoft Corporation.
 
 Permission to use, copy, modify, and/or distribute this software for any
