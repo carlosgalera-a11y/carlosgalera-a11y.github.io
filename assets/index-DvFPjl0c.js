c7954e0028f4490af209d945ece28fa52ec90a46
carlosg <carlosg@users.noreply.github.com>
2026-08-01 21:16:20 +0200

    deploy v242

--- assets/index-DvFPjl0c.js
+++ assets/index-DvFPjl0c.js
@@ -1,4 +1,4 @@
-import{a as is}from"./index-gHQ8sXjm.js";/*! *****************************************************************************
+import{a as is}from"./index-DqCpN880.js";/*! *****************************************************************************
 Copyright (c) Microsoft Corporation.
 
 Permission to use, copy, modify, and/or distribute this software for any
