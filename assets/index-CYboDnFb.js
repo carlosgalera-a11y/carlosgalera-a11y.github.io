f43d749a4c1762ec3fe8b9b9dc81af64b019059b
carlosg <carlosg@users.noreply.github.com>
2026-08-16 23:42:53 +0200

    deploy: WeLib en Libros/Lectura + lector epub (sw v310)

--- assets/index-CYboDnFb.js
+++ assets/index-CYboDnFb.js
@@ -1,4 +1,4 @@
-import{f as is}from"./index-Cg-BOiTd.js";/*! *****************************************************************************
+import{f as is}from"./index-NoQcra7w.js";/*! *****************************************************************************
 Copyright (c) Microsoft Corporation.
 
 Permission to use, copy, modify, and/or distribute this software for any
